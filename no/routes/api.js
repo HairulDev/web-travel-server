cc3fed6c455883038a53068a7653d5500a34c395
Hairul dev <khairullah.aplikasi@gmail.com>
2022-06-01 13:11:09 +0700

    make it better

--- no/routes/api.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const apiController = require('../controllers/apiController');
-
-router.post('/signin', apiController.actionSignin);
-module.exports = router;
